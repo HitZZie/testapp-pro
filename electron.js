ab8850d7e6c04c6ae57d203624e3326332fdb870
HitZZie <javisilc@gmail.com>
2025-07-16 19:46:37 +0200

    FIREBASE COMPLETO - formulario y TXT funcionando

---
+++ electron.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      webSecurity: false
+    },
+    titleBarStyle: 'default',
+    show: false,
+    title: 'TestApp Pro - Sistema de Oposiciones',
+    icon: path.join(__dirname, 'icon.png')
+  });
+
+  // Cargar la aplicación
+  const startUrl = isDev 
+    ? 'http://localhost:3000' 
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+  
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    mainWindow.focus();
+    
+    if (isDev) {
+      mainWindow.webContents.openDevTools();
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
